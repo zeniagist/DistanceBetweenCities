963bbf62ee63500b26836c60a8596d5c0a016080
Zenia Gist <zeniagist@gmail.com>
2020-12-23 18:23:16 +0000

    created app styling and container for map and distance output

--- index.php
+++ index.php
@@ -5,22 +5,70 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> 
+    
+    <!--Stylesheet-->
     <link href="styling.css" rel="stylesheet">
+    
+    <!--Google Fonts-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Arvo&display=swap" rel="stylesheet">
+
     <title>Distance Between Cities</title>
   </head>
   <body>
-
-    <!-- Optional JavaScript; choose one of the two! -->
+    <div class="jumbotron">
+        <div class="container-fluid">
+            <h1>Distance Between Cities</h1>
+            <p>Our app will help you find the traveling distance between two cities!</p>
+            
+            
+            <form class="form-horizontal">
+                <!--From City-->
+                <div class="form-group">
+                    <div class="col-xs-10">
+                        <label for="from" class="control-label col-xs-2">From:</label>
+                        <div div class="col-xs-10">
+                            <input type="text" id="from" placeholder="Origin" class="form-control">
+                        </div>
+                    </div>
+                </div>
+                
+                <!--To City-->
+                <div class="form-group">
+                    <div class="col-xs-10">
+                        <label for="to" class="col-xs-2 control-label">To:</label>
+                        <div div class="col-xs-10">
+                            <input type="text" id="to" placeholder="Destination" class="form-control">
+                        </div>
+                    </div>
+                </div>
+            </form>
+            
+            <!--Submit Button-->
+            <div class="col-xs-offset-2 col-xs-10">
+                <button class="btn btn-lg btn-info" onclick="calcRoute();">Submit</button>
+            </div>
+        </div>
+        
+        <!--Map Container-->
+        <div class="container-fluid">
+            <!--Map-->
+            <div id="googleMap">
+                
+            </div>
+            
+            <!--Distance Output-->
+            <div id="output">
+                
+            </div>
+        </div>
+    </div>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-    -->
   </body>
 </html>
